5eb97477abd531a4926ed1b3050bf7fc63087baa
Sil-Rei <silas.reiling@hotmail.de>
2023-02-11 17:06:51 +0100

    changed to debug on dev branch

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -27,7 +27,7 @@ from datetime import timedelta
 SECRET_KEY = 'django-insecure-4_k&+$x-2i+22x0rzw+p46+vm1@c^zh9in4ad+di#@1+6-_j5)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
